70a513ee1f418676a8e9255c09cfa3cbc408df59
Yehoonn <jes6208@naver.com>
2022-09-19 15:30:57 +0900

    리액트 express 연결

---
+++ src/API/Server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 4000;
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "..", "build", "/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port : ${port}`);
+});
+
+app.use(express.static(path.join(__dirname, "..", "..", "build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "..", "build", "/index.html"));
+});
